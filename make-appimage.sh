0bf583712d65cc3bddb5df37f1ef9c1aa9865d03
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-21 14:31:51 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,20 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q libation | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/libation.svg
+export DESKTOP=/usr/share/applications/Libation.desktop
+export DEPLOY_DOTNET=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/libation* \
+	/usr/bin/hangover  \
+	/usr/lib/libation
 
 # Additional changes can be done in between here
 
