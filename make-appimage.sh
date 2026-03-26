580353ad57d2992676f383d814d9c65cf61ec6f3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-26 04:56:20 -0400

    🧐

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/libation.svg
 export DESKTOP=/usr/share/applications/Libation.desktop
-#export DEPLOY_DOTNET=1 fails for some reason
+export DEPLOY_DOTNET=1
 export MAIN_BIN=libation
 
 # Deploy dependencies
